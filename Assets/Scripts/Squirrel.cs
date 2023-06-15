a0cd58a7b48650756834e813cdf6e334ad2199ab
Tomás Solano <tomas.solano@usach.cl>
2023-06-15 08:16:20 -0400

    Added GrabAcorns function to squirrel

--- Assets/Scripts/Squirrel.cs
+++ Assets/Scripts/Squirrel.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class Squirrel : MonoBehaviour
 {
-    
+    [SerializeField] float radius = 5f;
+    private void Update()
+    {
+        GrabAcorns();
+    }
+
+    private void GrabAcorns()
+    {
+        Collider[] colliders = Physics.OverlapSphere(this.transform.position, radius, 1<<10);
+        foreach (Collider collider in colliders)
+        {
+            Destroy(collider.gameObject);
+        }
+    }
 }
