91a9497f3eb7f13bcd56186be64047542ef86b3a
Tomás Solano <tomas.solano@usach.cl>
2023-06-18 22:39:50 -0400

    Stuff

--- Assets/Scripts/UIUpdater.cs
+++ Assets/Scripts/UIUpdater.cs
@@ -1,11 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class UIUpdater : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI acorns;
+
+    //Squirrel Menu
+    [Header("Squirrel")]
+    [SerializeField] Sprite squirrelHover;
+    [SerializeField] Sprite squirrelHoverDefault;
+    [SerializeField] Sprite squirrelClick;
+    [SerializeField] Image squirrelButtonImage;
+
+    //Tree Menu
+    [Header("Tree")]
+    [SerializeField] Sprite treeClick;
+    [SerializeField] Sprite treeDefaultSprite;
+    [SerializeField] Image treeButtonImage;
+
+
     private void Start()
     {
         EventManager.AddListener("UpdateAcornUI", UpdateAcorns);
@@ -15,5 +31,30 @@ public class UIUpdater : MonoBehaviour
     {
         string currentAcorns = ((int)acornsSize).ToString();
         acorns.text = currentAcorns;
+    }
+
+    public void HoverSquirrelMenuEnter()
+    {
+        squirrelButtonImage.sprite = squirrelHover;
+    }
+
+    public void HoverSquirrelMenuExit()
+    {
+        squirrelButtonImage.sprite = squirrelHoverDefault;
+    }
+
+    public void OnClickSquirrel()
+    {
+        squirrelButtonImage.sprite = squirrelClick;
+    }
+
+    public void OnClickTree()
+    {
+        treeButtonImage.sprite = treeClick;
+    }
+
+    public void HoverTreeMenuExit()
+    {
+        treeButtonImage.sprite = treeDefaultSprite;
     }
 }
