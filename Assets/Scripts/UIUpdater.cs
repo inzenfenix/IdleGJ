4961125e4045d7ca3316850e55e14d38bb8e8421
Tomás Solano <tomas.solano@usach.cl>
2023-06-25 17:01:48 -0400

    Update UIUpdater.cs

--- Assets/Scripts/UIUpdater.cs
+++ Assets/Scripts/UIUpdater.cs
@@ -272,7 +272,7 @@ public class UIUpdater : MonoBehaviour
             }
         }
 
-        squirrelPrices[index] = (int)Mathf.Pow(squirrelPrices[index],1.05f);
+        squirrelPrices[index] = (int)Mathf.Pow(squirrelPrices[index],1.1f);
         UpdateShopSingleSquirrel(buyableSquirrels[index], squirrel);
         UpdateAcorns();
     }
