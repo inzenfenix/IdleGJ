50cabb5d18fe7a11bdff59cea135e13cb65e41c7
Tomás Solano <tomas.solano@usach.cl>
2023-06-25 23:38:58 -0400

    gfdhsxn

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,7 +9,7 @@ public class GameManager : MonoBehaviour
     public static int currentLevel = 0;
     public static int acornsGrabbed = 0;
     [SerializeField] Transform buyPoint;
-    [SerializeField] int debugAcorns;
+    //[SerializeField] int debugAcorns;
 
     private void Start()
     {
@@ -20,7 +20,7 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-        acornsGrabbed = debugAcorns;
+        //acornsGrabbed = debugAcorns;
     }
     private void UpdateAcorns()
     {
