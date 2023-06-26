7f02f7915ee59eb11533dcbd18a9f77020ffed98
Tomás Solano <tomas.solano@usach.cl>
2023-06-26 16:35:15 -0400

    gvbcf

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] GameObject radio;
+    private Animator radioAnim;
+    [SerializeField] AudioMixer mixer;
+
+    private void Start()
+    {
+        
+    }
+}
